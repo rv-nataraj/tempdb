bfc26869716be9df95744e6c2f833ee8cfc78c83
rvnataraj <rv.nataraj@gmail.com>
2020-10-07 12:25:54 +0530

    first

---
+++ run.sh
@@ -0,0 +1,25 @@
+totalarg=$#
+if [ $totalarg -eq 1 ]
+then
+        echo "database configuration"
+        file=$1
+        i=1;
+        while read line; do
+                echo "$line"
+                sc[$i]="$line"
+                i=$((i+1))
+        done <$file
+	mkdir ~/temp/${sc[1]}
+	mkdir ~/temp/${sc[1]}/config
+	mkdir ~/temp/${sc[1]}/data
+
+	
+
+	docker kill ${sc[1]}
+	docker rm ${sc[1]}
+	sudo docker run -d --name=${sc[1]} -p ${sc[2]}:3306 -e TZ=Asia/Kolkata --restart=always \
+	    -v ~/temp/${sc[1]}/config:/etc/mysql/conf.d \
+	     -v ~/temp/${sc[1]}/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${sc[3]} mariadb
+else
+	echo  "dbconfig file missing"
+fi
